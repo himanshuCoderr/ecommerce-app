ac277c73493f23255dce46a0fb48f04d825bfd2f
Himanshu Malhotra <malhotrahimanshu002@gmail.com>
2024-09-03 17:04:20 +0530

    Update on File Uploads

--- src/Admin/Pages/AddProduct/AddProduct.jsx
+++ src/Admin/Pages/AddProduct/AddProduct.jsx
@@ -1,26 +1,29 @@
-
 import React, { useState } from 'react';
 import AdminNavbar from '../../Components/AdminNavbar/AdminNavbar';
-import {storage} from '../../../firebase' 
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from '../../../firebase'; 
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 function AddProduct({ addProduct }) {
   const [product, setProduct] = useState({
     name: '',
-    description:'',
+    description: '',
     price: '',
     quantity: '',
     dateOfMfg: '',
     image: ''
   });
 
-  // Create a child reference
-  const imagesRef = ref(storage, 'productThumbnail');
-  // imagesRef now points to 'images'
+  const [thumbnailImage, setThumbnailImage] = useState(null);
+  const [imageOne, setImageOne] = useState(null);
+  const [imageTwo, setImageTwo] = useState(null);
+  const [imageThree, setImageThree] = useState(null);
+  const [imageFour, setImageFour] = useState(null);
+  const [imageFive, setImageFive] = useState(null);
 
-  const [file, setFile] = useState(null);
   const [progress, setProgress] = useState(0);
   const [downloadURL, setDownloadURL] = useState("");
+
+  const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -30,29 +33,72 @@ function AddProduct({ addProduct }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     addProduct(product);
-    setProduct({ name: '', description:'', price: '', quantity:'', dateOfMfg: '', image: '' });
+    setProduct({ name: '', description: '', price: '', quantity: '', dateOfMfg: '', image: '' });
   };
 
-  // Handle file selection
+  // Handle file selection with file type validation
   const handleFileChange = (event) => {
+    console.log(event.target.name)
     const selectedFile = event.target.files[0];
-    if (selectedFile) {
-      setFile(selectedFile);
+    if (selectedFile && !allowedTypes.includes(selectedFile.type)) {
+      alert('Please select a valid image file (jpg, jpeg, png).');
+      return;
     }
-  };
+
+    let ctrlName = event.target.name;
+    if(ctrlName=='thumbnailImage') {
+      if (selectedFile) {
+        setThumbnailImage(selectedFile);
+      } 
+    } else if(ctrlName=='image1') {
+      if (selectedFile) {
+        setImageOne(selectedFile);
+      } 
+    } else if(ctrlName=='image2'){
+      if (selectedFile) {
+        setImageTwo(selectedFile);
+      } 
+    } else if(ctrlName=='image3'){
+      if (selectedFile) {
+        setImageThree(selectedFile);
+      } 
+    } else if(ctrlName=='image4'){
+      if (selectedFile) {
+        setImageFour(selectedFile);
+      } 
+    } else if(ctrlName=='image5'){
+      if (selectedFile) {
+        setImageFive(selectedFile);
+      } 
+    }
+};
 
   // Handle file upload to Firebase Storage
-  const handleUpload = () => {
-    if (!file) return;
-
-    const storageRef = ref(storage, `uploads/{file.name}`); // Create a reference to the file
-    const uploadTask = uploadBytesResumable(storageRef, file); // Upload file
+  const handleUpload = (event) => {
+    let btnName = event.target.name;
+    console.log(btnName);
+    let file = null;
+    if(btnName=='btnThumbnail') {
+      file = thumbnailImage;
+    } else if (btnName=='btnImage1'){
+      file = imageOne;
+    } else if (btnName=='btnImage2'){
+      file = imageTwo;
+    } else if (btnName=='btnImage3'){
+      file = imageThree;
+    } else if (btnName=='btnImage4'){
+      file = imageFour;
+    } else if (btnName=='btnImage5'){
+      file = imageFive;
+    }
+   
+     const storageRef = ref(storage, `uploads/${file.name}`); // Create a reference to the file
+     const uploadTask = uploadBytesResumable(storageRef, file); // Upload file
 
     // Monitor the upload process
     uploadTask.on(
       "state_changed",
       (snapshot) => {
-        // Calculate upload progress
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setProgress(progress);
       },
@@ -69,82 +115,80 @@ function AddProduct({ addProduct }) {
     );
   };
 
-
   return (
     <div className='' >
       <AdminNavbar />
-        <form onSubmit={handleSubmit} className='border-black border-2 p-5 m-5'>
-        <div className='mt-4' m-auto p-5  >
-          <label htmlFor="pn" className='font-bold mr-2 w-48 inline-block' >Product name</label>
+      <form onSubmit={handleSubmit} className='border-black border-2 p-5 m-5'>
+        <div className='mt-4' m-auto p-5>
+          <label htmlFor="pn" className='font-bold mr-2 w-48 inline-block'>Product name</label>
           <input
             id="pn"
-            type="text" 
-            name="name" 
-            placeholder="Product Name" 
-            value={product.name} 
-            onChange={handleChange} 
-            required 
-            className='border-2 border-black p-1 rounded-sm' 
+            type="text"
+            name="name"
+            placeholder="Product Name"
+            value={product.name}
+            onChange={handleChange}
+            required
+            className='border-2 border-black p-1 rounded-sm'
           />
-          </div>
-          <div className='mt-4'  >
-          <label htmlFor="pn" className='font-bold mr-2 w-48 inline-block' >Product Description</label>
-          <input 
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pd" className='font-bold mr-2 w-48 inline-block'>Product Description</label>
+          <input
             id="pd"
-            type="text" 
-            name="description" 
-            placeholder="Product Description" 
-            value={product.description} 
-            onChange={handleChange} 
-            required 
-            className='border-2 border-black p-1 rounded-sm' 
+            type="text"
+            name="description"
+            placeholder="Product Description"
+            value={product.description}
+            onChange={handleChange}
+            required
+            className='border-2 border-black p-1 rounded-sm'
           />
-          </div>
-          <div className='mt-4'  >
-          <label htmlFor="pn" className='font-bold mr-2 w-48 inline-block' >Product Price</label>
-          <input 
-            type="number" 
-            name="price" 
-            placeholder="Price" 
-            value={product.price} 
-            onChange={handleChange} 
-            required 
-            className='border-2 border-black p-1 rounded-sm' 
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold mr-2 w-48 inline-block'>Product Price</label>
+          <input
+            type="number"
+            name="price"
+            placeholder="Price"
+            value={product.price}
+            onChange={handleChange}
+            required
+            className='border-2 border-black p-1 rounded-sm'
           />
-          </div>
-          <div className='mt-4'  >
-          <label htmlFor="pn" className='font-bold mr-2 w-48 inline-block' >Product Quantity</label>
-          <input 
-            type="number" 
-            name="quantity" 
-            placeholder="Quantity" 
-            value={product.quantity} 
-            onChange={handleChange} 
-            required 
-            className='border-2 border-black p-1 rounded-sm' 
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold mr-2 w-48 inline-block'>Product Quantity</label>
+          <input
+            type="number"
+            name="quantity"
+            placeholder="Quantity"
+            value={product.quantity}
+            onChange={handleChange}
+            required
+            className='border-2 border-black p-1 rounded-sm'
           />
-          </div>
-          <div className='mt-4'  >
-          <label htmlFor="pn" className='font-bold w-48 inline-block' >Date of Mfg</label>
-          <input 
-            type="date" 
-            name="dateOfMfg" 
-            value={product.dateOfMfg} 
-            onChange={handleChange} 
-            required 
-            className='border-2 border-black p-1 rounded-sm' 
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold w-48 inline-block'>Date of Mfg</label>
+          <input
+            type="date"
+            name="dateOfMfg"
+            value={product.dateOfMfg}
+            onChange={handleChange}
+            required
+            className='border-2 border-black p-1 rounded-sm'
           />
-          </div>
-          <div className='mt-4'>
-          <label htmlFor="pn" className='font-bold w-48 inline-block' >Product Thumbnail </label>
-          <input type="file" name="image" onChange={handleFileChange} />
-          { file && (
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold w-48 inline-block'>Product Thumbnail</label>
+          <input type="file" name="thumbnailImage" onChange={handleFileChange} />
+          { thumbnailImage && (
             <div>
-              <button className="border-red-800" onClick={handleUpload}>Upload File</button>
-              <p>File Name: {file.name}</p>
+              <button className="border-red-800" name="btnThumbnail" type="button" onClick={handleUpload}>Upload File</button>
+              <p>File Name: {thumbnailImage.name}</p>
             </div>
-          ) }
-          
+          )}
           {progress > 0 && <p>Upload Progress: {Math.round(progress)}%</p>}
           {downloadURL && (
             <div>
@@ -154,33 +198,104 @@ function AddProduct({ addProduct }) {
               </a>
             </div>
           )}
-          </div>
-          <div className='mt-4'> 
-            <label htmlFor="Image1" className='font-bold w-48 inline-block'>Image 1</label>
-            <input type="file" name="Image1"></input>
-            <button>Upload</button>
-          </div>
-          <div className='mt-4'> 
-            <label htmlFor="Image2" className='font-bold w-48 inline-block'>Image 2</label>
-            <input type="file" name="Image2"></input>
-            <button>Upload</button>
-          </div>
-          <div className='mt-4'> 
-            <label htmlFor="Image3" className='font-bold w-48 inline-block'>Image 3</label>
-            <input type="file" name="Image3"></input>
-            <button>Upload</button>
-          </div>
-          <div className='mt-4'> 
-            <label htmlFor="Image4" className='font-bold w-48 inline-block'>Image 4</label>
-            <input type="file" name="Image4"></input>
-            <button>Upload</button>
-          </div>
-          <div className='mt-4'>
-            {/* <ImageUrl/> */}
-          </div>
-
-          <button type="submit" className='mt-4 block p-1 rounded-md border-[1px] shadow-md text-sm' >Add Product</button>
-        </form>
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold w-48 inline-block'>Product Image 1</label>
+          <input type="file" name="image1" onChange={handleFileChange} />
+          { imageOne && (
+            <div>
+              <button className="border-red-800" name="btnImage1" type="button" onClick={handleUpload}>Upload File</button>
+              <p>File Name: {imageOne.name}</p>
+            </div>
+          )}
+          {progress > 0 && <p>Upload Progress: {Math.round(progress)}%</p>}
+          {downloadURL && (
+            <div>
+              <p>File uploaded successfully! You can access it here:</p>
+              <a href={downloadURL} target="_blank" rel="noopener noreferrer">
+                {downloadURL}
+              </a>
+            </div>
+          )}
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold w-48 inline-block'>Product Image 2</label>
+          <input type="file" name="image2" onChange={handleFileChange} />
+          { imageTwo && (
+            <div>
+              <button className="border-red-800" name="btnImage2" type="button" onClick={handleUpload}>Upload File</button>
+              <p>File Name: {imageTwo.name}</p>
+            </div>
+          )}
+          {progress > 0 && <p>Upload Progress: {Math.round(progress)}%</p>}
+          {downloadURL && (
+            <div>
+              <p>File uploaded successfully! You can access it here:</p>
+              <a href={downloadURL} target="_blank" rel="noopener noreferrer">
+                {downloadURL}
+              </a>
+            </div>
+          )}
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold w-48 inline-block'>Product Image 3</label>
+          <input type="file" name="image3" onChange={handleFileChange} />
+          { imageThree && (
+            <div>
+              <button className="border-red-800" type="button" name="btnImage3" onClick={handleUpload}>Upload File</button>
+              <p>File Name: {imageThree.name}</p>
+            </div>
+          )}
+          {progress > 0 && <p>Upload Progress: {Math.round(progress)}%</p>}
+          {downloadURL && (
+            <div>
+              <p>File uploaded successfully! You can access it here:</p>
+              <a href={downloadURL} target="_blank" rel="noopener noreferrer">
+                {downloadURL}
+              </a>
+            </div>
+          )}
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold w-48 inline-block'>Product Image 4</label>
+          <input type="file" name="image4" onChange={handleFileChange} />
+          { imageFour && (
+            <div>
+              <button className="border-red-800" type="button" name="btnImage4" onClick={handleUpload}>Upload File</button>
+              <p>File Name: {imageFour.name}</p>
+            </div>
+          )}
+          {progress > 0 && <p>Upload Progress: {Math.round(progress)}%</p>}
+          {downloadURL && (
+            <div>
+              <p>File uploaded successfully! You can access it here:</p>
+              <a href={downloadURL} target="_blank" rel="noopener noreferrer">
+                {downloadURL}
+              </a>
+            </div>
+          )}
+        </div>
+        <div className='mt-4'>
+          <label htmlFor="pn" className='font-bold w-48 inline-block'>Product Image 5</label>
+          <input type="file" name="image5" onChange={handleFileChange} />
+          { imageFive && (
+            <div>
+              <button className="border-red-800" type="button" name="btnImage5" onClick={handleUpload}>Upload File</button>
+              <p>File Name: {imageFive.name}</p>
+            </div>
+          )}
+          {progress > 0 && <p>Upload Progress: {Math.round(progress)}%</p>}
+          {downloadURL && (
+            <div>
+              <p>File uploaded successfully! You can access it here:</p>
+              <a href={downloadURL} target="_blank" rel="noopener noreferrer">
+                {downloadURL}
+              </a>
+            </div>
+          )}
+        </div>
+        <button type="submit" className='mt-4 block p-1 rounded-md border-[1px] shadow-md text-sm'>Add Product</button>
+      </form>
     </div>
   );
 }
